a0fcc2d4196e854cb4602b2b26f5d897b11e4ec2
Mik Massotto <mm2529@njit.edu>
2022-10-27 11:47:12 -0400

    moving login and logout pages to the php folder, updated filespaths

--- web/public_html/php/login.php
+++ web/public_html/php/login.php
@@ -1,8 +1,8 @@
 
 <?php
-require_once('../path.inc');
-require_once('../get_host_info.inc');
-require_once('../rabbitMQLib.inc');
+require_once('../../path.inc');
+require_once('../../get_host_info.inc');
+require_once('../../rabbitMQLib.inc');
 
 //login();
 
