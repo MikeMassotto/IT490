66bf02249344693024781093cc063b5b6981024c
Mik Massotto <mm2529@njit.edu>
2022-10-31 18:33:31 -0400

    IT WAS THE POSITION OF THE FUNCTION CALL ALL ALONG

--- web/public_html/php/user_request.php
+++ web/public_html/php/user_request.php
@@ -1,11 +1,32 @@
 <?php
 
-   function get_all_user_data($userid)
+   function get_profile_info($userid)
    {
 
       $client = new rabbitMQClient("../testRabbitMQ.ini","testserver");
       $request = array();
       $request['type'] = "get_user_data";
+      $request['user_id'] = $userid;
+      echo json_encode($request);
+
+      $response = $client ->send_request($request);
+
+      if($response)
+      {
+         echo $response;
+         return $response;
+      }
+
+      return "No response.";
+
+   }
+
+   function get_friends_list($userid)
+   {
+
+      $client = new rabbitMQClient("../testRabbitMQ.ini","testserver");
+      $request = array();
+      $request['type'] = "get_friends_list";
       $request['user_id'] = $userid;
       echo json_encode($request);
 
@@ -28,7 +49,11 @@
    switch ($request["type"]){
 
       case "all":
-         $response = get_all_user_data($request["userid"]);
+         $response = get_profile_info($request["userid"]);
+         break;
+
+      case "friends_list":
+         $response = get_friends_list($request["userid"]);
          break;
    }
 
