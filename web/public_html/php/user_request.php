a62aab7b7d94bd20ad07d45224ac5c49563dd117
Mik Massotto <mm2529@njit.edu>
2022-11-02 21:30:33 -0400

    the settings code is an inefficient, gross, mess. but it MIGHT work

--- web/public_html/php/user_request.php
+++ web/public_html/php/user_request.php
@@ -42,6 +42,48 @@
 
    }
 
+   function true_false_to_1_0($bool)
+   {
+      if($bool)
+      {
+         return 1;
+      }
+      else
+      {
+         return 0;
+      }
+   }
+
+   function update_settings($privacy)
+   {
+      $client = new rabbitMQClient("../testRabbitMQ.ini","testserver");
+      
+      //Update profile privacy
+      $request = array();
+      $request['type'] = "user_update_profile_privacy";
+      $request['user_id'] = $_SESSION['userid'];
+      $request['public'] = true_false_to_1_0($privacy[0]);
+      $response = $client ->send_request($request);
+      echo json_encode($response);
+
+      //Update friend privacy
+      $request = array();
+      $request['type'] = "user_update_friends_public";
+      $request['user_id'] = $_SESSION['userid'];
+      $request['public'] = true_false_to_1_0($privacy[1]);
+      $response = $client ->send_request($request);
+      echo json_encode($response);
+
+      //Update achievements privacy
+      $request = array();
+      $request['type'] = "user_update_achievements_public";
+      $request['user_id'] = $_SESSION['userid'];
+      $request['public'] = true_false_to_1_0($privacy[2]);
+      $response = $client ->send_request($request);
+      echo json_encode($response);
+
+   }
+
    $request = $_POST;
 
    //Switch statement handles all user requests from here
@@ -54,6 +96,10 @@
 
       case "friends_list":
          $response = get_friends_list($request["userid"]);
+         break;
+
+      case "settings":
+         $response = update_settings($request["privacy"]);
          break;
    }
 
